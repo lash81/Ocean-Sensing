d14a96cb88bc530ce1bf3bd6414b3188d0fcd6bc
Lauren Shum <laurenshum@Laurens-MacBook-Pro.local>
2017-03-24 00:07:17 -0400

    comment out turbidity to measure throughput only; serial print only green vals

--- colorimetry_nowifi/colorimetry_nowifi.ino
+++ colorimetry_nowifi/colorimetry_nowifi.ino
@@ -6,7 +6,7 @@ Adafruit_TCS34725 tcs;
 uint16_t r, g, b, c, colorTemp, lux = 0;  // define values for color sensor readings, initialize to 0
 int turbiditySensor = 12;  // pin 12 = Turbidity color sensor VDD
 int throughputSensor = 14;  // pin 14 = Throughput color sensor VDD
-int NUM_DATA = 20; // number of data points to collect
+int NUM_DATA = 101; // number of data points to collect
 
 void setup(void) {
   Serial.begin(115200);
@@ -33,30 +33,31 @@ void loop(void) {
       delay(750);
     }
 
-    digitalWrite(throughputSensor, LOW);
-    Serial.println("Finished throughput readings. Initializing Turbidity readings...");
-    delay(2000);  // Extra delay just in case
+    Serial.println("FINISHED WITH DATA COLLECTION.");
 
     // TURBIDITY READINGS
-    tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);  // initialize sensor values each time the function gets called
-    digitalWrite(turbiditySensor, HIGH);
-    delay(2000); // Need to wair for sensor to initialize itself
-
-    if (tcs.begin()) {
-      Serial.println("Found turbidity color sensor!");
-    } else {
-      Serial.println("Turbidity sensor could not be found ... check your connections");
-      while (1);
-    }
-
-    for(int j = 0; j < NUM_DATA; j++){
-      sensorON(turbiditySensor);
-      delay(750);
-    }
-
-    digitalWrite(turbiditySensor, LOW);
-    delay(2000);
-  
+//    digitalWrite(throughputSensor, LOW);
+//    Serial.println("Finished throughput readings. Initializing Turbidity readings...");
+//    delay(2000);  // Extra delay just in case
+//    tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);  // initialize sensor values each time the function gets called
+//    digitalWrite(turbiditySensor, HIGH);
+//    delay(2000); // Need to wair for sensor to initialize itself
+//
+//    if (tcs.begin()) {
+//      Serial.println("Found turbidity color sensor!");
+//    } else {
+//      Serial.println("Turbidity sensor could not be found ... check your connections");
+//      while (1);
+//    }
+//
+//    for(int j = 0; j < NUM_DATA; j++){
+//      sensorON(turbiditySensor);
+//      delay(750);
+//    }
+//
+//    digitalWrite(turbiditySensor, LOW);
+//    delay(2000);
+    // END TURBIDITY READINGS
 }
 
 // create function to call the sensor depending on the pin desired
@@ -68,12 +69,14 @@ void sensorON(int pin){  // "pin" is determined in the main loop, and is the pin
   lux = tcs.calculateLux(r, g, b);
   
   // display data
-  Serial.print("Pin: "); Serial.print(pin, DEC);
-  Serial.print(" Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
-  Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
-  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
-  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
-  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
-  Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
-  Serial.println(" ");
+//  Serial.print("Pin: "); Serial.print(pin, DEC);
+//  Serial.print(" Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
+//  Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
+//  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
+//  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
+//  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
+//  Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
+//  Serial.println(" ");
+  Serial.println(g, DEC);
+  
 }
