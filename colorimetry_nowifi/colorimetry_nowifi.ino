5a2c07b3aade0e5b0ded05db4489314d6fa19e43
as515@duke.edu <as515@duke.edu>
2017-04-12 21:55:06 -0400

    ph sensor

--- colorimetry_nowifi/colorimetry_nowifi.ino
+++ colorimetry_nowifi/colorimetry_nowifi.ino
@@ -6,7 +6,7 @@ Adafruit_TCS34725 tcs;
 uint16_t r, g, b, c, colorTemp, lux = 0;  // define values for color sensor readings, initialize to 0
 int turbiditySensor = 12;  // pin 12 = Turbidity color sensor VDD
 int throughputSensor = 14;  // pin 14 = Throughput color sensor VDD
-int NUM_DATA = 20; // number of data points to collect
+int NUM_DATA = 1000; // number of data points to collect
 
 void setup(void) {
   Serial.begin(115200);
@@ -15,27 +15,27 @@ void setup(void) {
 }
 
 void loop(void) {
-    // THROUGHPUT READINGS
-    tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
-    Serial.println("Initializing Throughput Readings...");
-    digitalWrite(throughputSensor, HIGH);
-    delay(2000); // Need to delay to wait for sensor to initialize itself
-
-    if (tcs.begin()) {
-      Serial.println("Found throughput color sensor!");
-    } else {
-      Serial.println("Throughput sensor could not be found ... check your connections");
-      while (1);
-    }
-
-    for(int i = 0; i < NUM_DATA; i++){
-      sensorON(throughputSensor);
-      delay(750);
-    }
-
-    digitalWrite(throughputSensor, LOW);
-    Serial.println("Finished throughput readings. Initializing Turbidity readings...");
-    delay(2000);  // Extra delay just in case
+//    // THROUGHPUT READINGS
+//    tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
+//    Serial.println("Initializing Throughput Readings...");
+//    digitalWrite(throughputSensor, HIGH);
+//    delay(2000); // Need to delay to wait for sensor to initialize itself
+//
+//    if (tcs.begin()) {
+//      Serial.println("Found throughput color sensor!");
+//    } else {
+//      Serial.println("Throughput sensor could not be found ... check your connections");
+//      while (1);
+//    }
+//
+//    for(int i = 0; i < NUM_DATA; i++){
+//      sensorON(throughputSensor);
+//      delay(750);
+//    }
+//
+//    digitalWrite(throughputSensor, LOW);
+//    Serial.println("Finished throughput readings. Initializing Turbidity readings...");
+//    delay(2000);  // Extra delay just in case
 
     // TURBIDITY READINGS
     tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);  // initialize sensor values each time the function gets called
@@ -51,7 +51,7 @@ void loop(void) {
 
     for(int j = 0; j < NUM_DATA; j++){
       sensorON(turbiditySensor);
-      delay(750);
+      delay(700);
     }
 
     digitalWrite(turbiditySensor, LOW);
@@ -68,12 +68,5 @@ void sensorON(int pin){  // "pin" is determined in the main loop, and is the pin
   lux = tcs.calculateLux(r, g, b);
   
   // display data
-  Serial.print("Pin: "); Serial.print(pin, DEC);
-  Serial.print(" Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
-  Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
-  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
-  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
-  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
-  Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
-  Serial.println(" ");
+  Serial.println(g, DEC);
 }
